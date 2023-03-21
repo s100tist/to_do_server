fcbbdf534aad5d44db7f6b63d2bda86bd1d9648e
Esme Pérez <mtz.esme9@gmail.com>
2023-03-20 22:14:43 -0600

    Actualización crear nota

--- src/componentes/nota/crear_nota.dto.ts
+++ src/componentes/nota/crear_nota.dto.ts
@@ -1,12 +1,12 @@
 export class Crear_nota_dto{
     id_propietario_nota:    number;
-    compartida_nota:        boolean;
+    compartida_nota?:        boolean;
     titulo_nota:            string;
     contenido_nota:         string;
     id_categoria_nota:      number;
-    prioridad_nota:         number;  
+    prioridad_nota?:         number;  
     color_nota:             string; 
     fecha_creacion_nota:    Date;
     fecha_termino_nota:     Date;
-    finalizado_nota:        boolean;
+    finalizado_nota?:        boolean;
 }
